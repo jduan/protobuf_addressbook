495e14af13c1374d3620aa1239fc86b6065bbc16
Jingjing Duan <jduan@twitter.com>
2015-08-27 22:28:04 -0700

    tidy it up

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import sys
 import addressbook_pb2
 
 
-def PromptForAddress(person):
+def prompt_for_address(person):
   person.id = int(raw_input("Enter person ID number: "))
   person.name = raw_input("Enter person name: ")
   email = raw_input("Enter person email (blank for none): ")
@@ -26,27 +26,26 @@ def PromptForAddress(person):
     elif type == "work":
       phone_number.type = addressbook_pb2.Person.WORK
     else:
-      print "Unknown phone type; leaving as default value."
+      print("Unknown phone type; leaving as default value.")
 
 
 if len(sys.argv) != 2:
-  print "Usage: %s ADDRESS_BOOK_FILE" % sys.argv[0]
+  print("Usage: %s ADDRESS_BOOK_FILE" % sys.argv[0])
   sys.exit(-1)
 
 address_book = addressbook_pb2.AddressBook()
+address_book_file = sys.argv[1]
 
 # Read the existing address book
 try:
-  f = open(sys.argv[1], "rb")
-  address_book.ParseFromString(f.read())
-  f.close()
+  with open(address_book_file, "rb") as fd:
+    address_book.ParseFromString(fd.read())
 except IOError:
-  print "Couldn't open file. Creating a new one"
+  print("Couldn't open file. Creating a new one")
 
 # add an address
-PromptForAddress(address_book.person.add())
+prompt_for_address(address_book.person.add())
 
 # write the new address book back to disk
-f = open(sys.argv[1], "wb")
-f.write(address_book.SerializeToString())
-f.close()
+with open(address_book_file, "wb") as fd:
+  fd.write(address_book.SerializeToString())
